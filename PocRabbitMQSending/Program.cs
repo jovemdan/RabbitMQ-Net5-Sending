73422c92451657af17d54e569d3f0987d0df22bf
jovemdan <danilogdev@gmail.com>
2022-01-25 16:59:37 -0300

    first commit

---
+++ PocRabbitMQSending/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace PocRabbitMQSending
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "qwebapp",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                int count = 0;
+                while (true)
+                {
+                    string message = $"{count++} Ping!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "qwebapp",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                    System.Threading.Thread.Sleep(200); // a cada um segundo manda 5 msg
+                }
+    
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
